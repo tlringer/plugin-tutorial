388242136ca7ffdd5f82991944d9e88cba4da39f
Talia Ringer <talia@dependenttyp.es>
2022-03-04 18:33:19 -0600

    sketch out sequence of three commands

--- repair/theories/Demo.v
+++ repair/theories/Demo.v
@@ -1,4 +1,17 @@
+From Tuto1 Require Import Loader.
 From Tuto2 Require Import Loader.
+
+(*
+ * TODO explain and test the map command
+ *)
+
+(*
+ * TODO explain and test the configure swap command
+ *)
+
+(*
+ * TODO test composing with the sub command
+ *)
 
 (*
  * TODO explain and test the swap command
